9b7f9d308a92692f12cdca365ab79a09e5c6b078
Spencer Ryals <spencerryals@gmail.com>
2014-07-21 12:11:54 -0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -27,22 +27,21 @@ describe "Authentication" do
 		end 
 
 		describe "with valid information" do
-      		let(:user) { FactoryGirl.create(:user) }
-      		before { sign_in user }
+  		let(:user) { FactoryGirl.create(:user) }
+  		before { sign_in user }
 
-      		it { should have_title(user.name) }
-      		it { should have_link('Users',		   href: users_path) }
-      		it { should have_link('Profile',     href: user_path(user)) }
-      		it { should have_link('Settings',	   href: edit_user_path(user)) }
-      		it { should have_link('Sign out',    href: signout_path) }
-      		it { should_not have_link('Sign in', href: signin_path) }
+  		it { should have_title(user.name) }
+  		it { should have_link('Users',		   href: users_path) }
+  		it { should have_link('Profile',     href: user_path(user)) }
+  		it { should have_link('Settings',	   href: edit_user_path(user)) }
+  		it { should have_link('Sign out',    href: signout_path) }
+  		it { should_not have_link('Sign in', href: signin_path) }
 
-      		describe "followed by signout" do
-      			before { click_link "Sign out" }
-      			it { should have_link('Sign in') }
-      		end 
-    	end
-  	end
+  		describe "followed by signout" do
+  			before { click_link "Sign out" }
+  			it { should have_link('Sign in') }
+  		end 
+    end 
 
   	describe "authorization" do
 
@@ -76,7 +75,19 @@ describe "Authentication" do
   						expect(page).to have_title('Edit user')
   					end 
   				end 
-  			end 
+  			end
+
+        describe "in the Relationships controller" do
+          describe "submitting to the create action" do
+            before { post relationships_path }
+            specify { expect(response).to redirect_to(signin_path) }
+          end 
+
+          describe "submitting to the destroy action" do
+            before { delete relationship_path(1) }
+            specify { expect(response).to redirect_to(signin_path) }
+          end 
+        end 
 
   			describe "in the Users controller" do
 
@@ -89,6 +100,16 @@ describe "Authentication" do
   					before { patch user_path(user) }
   					specify { expect(response).to redirect_to(signin_path) }
   				end 
+
+          describe "visiting the following page" do
+            before { visit following_user_path(user) }
+            it { should have_title('Sign in') }
+          end 
+
+          describe "visiting the followers page" do
+            before { visit followers_user_path(user) }
+            it { should have_title('Sign in') }
+          end 
 
   				describe "visiting the user index" do
   					before { visit users_path }
@@ -113,6 +134,7 @@ describe "Authentication" do
   				specify { expect(response).to redirect_to(root_url) }
   			end
   		end  
+    end 
 
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
